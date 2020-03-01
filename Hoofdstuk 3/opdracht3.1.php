1799fc5075c0ecc2f5104607bc30a3a6b1e8066a
Raoel van Schaijk <Raoel.vanschaijk@edu-kw1c.nl>
2020-03-01 23:16:11 +0100

    correctie door docent

---
+++ Hoofdstuk 3/opdracht3.1.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: Raoel van Schaijk
+ * Date: 05/02/2020
+ * Time: 10:56 PM
+ * File: opdracht 3.1.php
+ */
+?>
+<link href= "index.php">
+<?php
+//initialisatie van variabelen
+$evenement = "elfstedentocht";
+$fries = "Alvestêdetocht";
+$kilometers = "200";
+$typebaan = "schaatstocht";
+$typestructuur = "natuurijs";
+$organisatie = "Koninklijke Vereniging De Friesche Elf Steden";
+$plaats = "Leeuwarden";
+$provincie = "Friesland";
+$evenementhoeveelheden = "15";
+$jaartijd = "1909";
+$aantal = "1";
+
+$verhaal1= "De $evenement (Fries: $fries) is een $kilometers lange $typebaan over $typestructuur die wordt georganiseerd
+door $organisatie. $plaats de hoofdstad van $provincie, is start- en aankomstplaats. De $evenement, is inmiddels $evenementhoeveelheden
+maal verreden en werd voor het eerst in $jaartijd gereden en wordt maximaal $aantal keer per winter gehouden.";
+
+//concat
+$verhaal2 = "de ".$evenement."(Fries: ".$fries. "is een".$kilometers."lange".$typebaan."over".$typestructuur."die wordt georganiseerd door".$organisatie.".".$plaats."de hoofdstad van".$provincie.", is start- en aankomstplaats. De".$evenement.", is inmiddels".$evenementhoeveelheden."maal verreden en werd voor het eerst in".$jaartijd."gereden en wordt maximaal".$aantal."keer per winter gehouden.";
+
+
+?>
+<!doctype html>
+<html>
+<body>
+<?php
+echo "<p> $verhaal1 </p>";
+echo "<p> $verhaal2 </p>";
+?>
+</body>
+</html>
